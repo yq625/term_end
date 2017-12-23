441a9b255a4737bc2df325749cfc2704b0600f6b
yq625 <yq625@nyu.edu>
2017-12-22 19:05:55 -0500

    Final_version:add abline(the bonus part) to the files

--- tetrahymena_PART_B_yq625.py
+++ tetrahymena_PART_B_yq625.py
@@ -16,4 +16,19 @@ y1 = data['log(conc)']['glucose_yes']
 y2 = data['log(conc)']['glucose_no']
 plt.scatter(x1, y1, c='b', marker = 'x')
 plt.scatter(x2, y2, c='r', marker = 's')
+[slope1, intercept1] = list(np.polyfit(x1,y1,deg = 1))
+[slope2, intercept2] = list(np.polyfit(x2,y2,deg = 1))
+# print(type(slope1))
+def abline(slope, intercept, symbol):
+    """Plot a line from slope and intercept"""
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, symbol)
+
+abline(slope1, intercept1, "-")
+abline(slope2, intercept2, "--")
 plt.show()
+
+
+
--- tetrahymena_part_A_yq625.R
+++ tetrahymena_part_A_yq625.R
@@ -9,7 +9,12 @@ df<-df %>%
 df<-df %>%
   mutate(log(`mean(conc)`),log(`mean(diameter)`))
 plot<-df %>%
-  ggplot(.,aes(x=log(`mean(diameter)`),y=log(`mean(conc)`)))+
-  geom_point(aes(color=glucose))+
-  geom_abline()
+  ggplot(.,aes(x=log(`mean(diameter)`),y=log(`mean(conc)`),shape=glucose))+
+  geom_point(aes(color=glucose))
 plot
+colnames(df)[5]<-'log_conc'
+colnames(df)[6]<-'log_dia'
+fit1<-lm(log_conc~log_dia, data = df[1:32,])
+fit2<-lm(log_conc~log_dia, data = df[33:51,])
+plot+geom_abline(intercept = coef(fit1)[1],slope = coef(fit1)[2],color='blue')+
+  geom_abline(intercept = coef(fit2)[1],slope = coef(fit2)[2],color='red')
